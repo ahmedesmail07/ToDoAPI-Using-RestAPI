571b7223178810d84430474fdf31f5e14cb7661f
ahmedesmail07 <ahmedismail332211@gmail.com>
2023-03-17 22:59:23 +0200

    Adding Views,serialzers and test

--- todos/tests.py
+++ todos/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from .models import Todo
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 
 class TodoModelTest(TestCase):
@@ -10,4 +13,17 @@ class TodoModelTest(TestCase):
     def test_model_content(self):
         self.assertEqual(self.todo.title, "Ending Rest")
         self.assertEqual(self.todo.body, "before 20-3-2023")
-        # self.assertEqual(str(self.todo),"Ending Rest")
+
+    def test_api_listview(self):
+        response = self.client.get(reverse("todo_list"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, self.todo.title)
+
+    def test_api_detailview(self):
+        response = self.client.get(
+            reverse("todo_detail", kwargs={"pk": self.todo.id}), format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Todo.objects.count(), 1)
+        self.assertContains(response, "Ending Rest")
